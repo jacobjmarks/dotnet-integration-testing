86c7df7fccb9997e03b18fb2fe4dc2b28c6c84ad
Zi Choong <zi.choong@azenix.com.au>
2023-07-07 15:01:22 +1000

    Add Book model and corresponding controller classes. Todo: Fix persistence issue between requests and calls.

---
+++ Reference A - No external dependencies/net6.0/src/Azenix.Examples.IntegrationTesting.Api/Controllers/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Azenix.Examples.IntegrationTesting.Api.Controllers;
+
+public class Book
+{
+    public string? Author { get; set; }
+    public string? Title { get; set; }
+    public string? Isbn { get; set; }
+    public string? Id { get; set; }
+    public DateTime UtcDatePublished { get; set; }
+    public DateTime UtcDateAdded { get; set; }
+}
---
+++ Reference A - No external dependencies/net6.0/src/Azenix.Examples.IntegrationTesting.Api/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Azenix.Examples.IntegrationTesting.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/books")]
+public class BookController : ControllerBase
+{
+    private readonly ILogger<BookController> _logger;
+    public static IList<Book> _bookStore;
+
+    public BookController(ILogger<BookController> logger)
+    {
+        _logger = logger;
+        _bookStore = new List<Book>();
+    }
+
+    [HttpGet("")]
+    public IEnumerable<Book> GetAllBooks()
+    {
+        _logger.LogInformation("Successfully returned all books");
+        return _bookStore;
+    }
+
+    [HttpGet("{id}")]
+    public List<Book> GetBook(string id)
+    {
+        _logger.LogInformation("Successfully returned a book with id: {Id}", id);
+        return _bookStore.Where(item => item.Id == id).ToList();
+    }
+
+    [HttpPost("{id}")]
+    public void CreateBook([FromBody] Book book, string id)
+    {
+        _logger.LogInformation("Successfully created a book with id: {Id}", id);
+        var item = new Book
+        {
+            Author = book.Id,
+            Title = book.Id,
+            UtcDatePublished = DateTime.MinValue,
+            UtcDateAdded = DateTime.UtcNow,
+            Isbn = book.Id,
+            Id = id
+        };
+        _bookStore.Add(item);
+    }
+
+    // [HttpPut(Name = "UpdateBook")]
+    // public void UpdateBook()
+    // {
+    //     
+    // }
+    //
+    // [HttpDelete(Name = "DeleteBook")]
+    // public void DeleteBook()
+    // {
+    //     
+    // }
+}
