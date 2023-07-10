338bc8d3324fe325600bf805a3927ec325db1bf7
Jacob Marks <jacob.marks@azenix.com.au>
2023-07-10 12:05:25 +1000

    Fix test

--- Reference A - No external dependencies/net6.0/test/Azenix.Examples.IntegrationTesting.Api.Tests/Tests.cs
+++ Reference A - No external dependencies/net6.0/test/Azenix.Examples.IntegrationTesting.Api.Tests/Tests.cs
@@ -4,19 +4,19 @@ namespace Azenix.Examples.IntegrationTesting.Api.Tests;
 
 public class Tests
 {
-    [Fact(Skip = "Skip")]
-    public async Task Get_WeatherForecast_Returns_200OK()
+    [Fact]
+    public async Task GetAllBooks_Returns_200OK()
     {
         //- arrange
         using var factory = new CustomWebApplicationFactory();
         using var client = factory.CreateClient();
 
         //- act
-        using var response = await client.GetAsync("WeatherForecast");
+        using var response = await client.GetAsync("Book/books");
         var responseContent = await response.Content.ReadAsStringAsync();
 
         //- assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        responseContent.Should().NotBeNullOrEmpty();
+        responseContent.Should().MatchSnapshot();
     }
 }
