a4bf508b245d1612c1372db392c9fa6ad3c2cdd0
Jacob Marks <jacob.marks@azenix.com.au>
2023-07-11 12:43:55 +1000

    Simplify Controller and example entity

--- Reference A - No external dependencies/net6.0/Example.Api.Tests/Tests.cs
+++ Reference A - No external dependencies/net6.0/Example.Api.Tests/Tests.cs
@@ -5,14 +5,14 @@ namespace Azenix.Examples.IntegrationTesting.Api.Tests;
 public class Tests
 {
     [Fact]
-    public async Task GetAllBooks_Returns_200OK()
+    public async Task GetEntities_Returns_200OK()
     {
         //- arrange
         using var factory = new CustomWebApplicationFactory();
         using var client = factory.CreateClient();
 
         //- act
-        using var response = await client.GetAsync("Book/books");
+        using var response = await client.GetAsync("entities");
         var responseContent = await response.Content.ReadAsStringAsync();
 
         //- assert
---
+++ Reference A - No external dependencies/net6.0/Example.Api/Models/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace Azenix.Examples.IntegrationTesting.Api.Models;
+
+public record class Entity
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset LastModifiedAt { get; set; }
+}
+
+public record class CreateEntityRequestBody
+{
+    public string Name { get; set; } = null!;
+}
+
+public record class UpdateEntityRequestBody
+{
+    public string Name { get; set; } = null!;
+}
